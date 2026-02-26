7764e0d5b7fcb1f9be9468564078ac66814c1dd7
kunnikarB <kunnikarboon@gmail.com>
2026-02-26 17:15:08 +0700

    update menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav');
 
 toggle?.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+  nav.classList.toggle('show');
 });
 
 const hoursBtn = document.getElementById('hoursBtn');
